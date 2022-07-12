577f946ffd4c78e5eb6c19357854f9ecdf93fd8f
Vic Tor <you@example.com>
2022-07-12 16:14:53 +0300

    deploying errors

--- components/Post.jsx
+++ components/Post.jsx
@@ -34,7 +34,7 @@ const Post = ({ name, message, email, postImage, image, timestamp }) => {
       </div>
       {postImage && (
         <div className="relative h-56 md:h-96 bg-white">
-          <Image src={postImage} objectFit="cover" layout="fill" />
+          <Image src={postImage} objectFit="cover" layout="fill" alt="/" />
         </div>
       )}
       <div
